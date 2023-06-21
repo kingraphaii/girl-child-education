0bed81e07507312e9744cd7deb4cfe34878f7627
Percival <percival.rapha@gmail.com>
2023-06-21 22:12:37 +0200

    Vercel Analytics

--- src/routes/(miss-kathu)/miss-kathu-beauty-peagent/+layout.svelte
+++ src/routes/(miss-kathu)/miss-kathu-beauty-peagent/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import '$lib/app.css';
 	import { Crown } from 'svelte-iconoir';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <nav
