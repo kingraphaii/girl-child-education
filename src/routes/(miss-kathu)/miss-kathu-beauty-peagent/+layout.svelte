ef7ee9cfed267fa4a8e8271bacc01241cb6a597a
Percival <percival.rapha@gmail.com>
2023-06-21 22:07:16 +0200

    Vercel Analytics

--- src/routes/(miss-kathu)/miss-kathu-beauty-peagent/+layout.svelte
+++ src/routes/(miss-kathu)/miss-kathu-beauty-peagent/+layout.svelte
@@ -10,7 +10,7 @@
 	<a href="/" class="inline-flex items-center justify-center gap-1 text-lg rounded-lg">
 		<span class="sr-only">Logo</span>
 		<Crown class="h-8 w-8 -rotate-12 text-pink-600" />
-		Miss Kathu Beauty Peagent
+		Miss Kathu <span class="hidden md:inline-block">Beauty Peagent</span>
 	</a>
 
 	<ul class="flex items-center gap-2 text-sm font-medium text-gray-500">
@@ -22,7 +22,7 @@
 
 		<li>
 			<a class="inline-flex bg-pink-600 text-white items-center gap-2 rounded px-3 py-2" href="/">
-				&larr; Girl Child Education
+				&larr; Girl Child <span class="hidden md:inline-block">Education</span>
 			</a>
 		</li>
 	</ul>
